a31bf78a9717886435d39d4a12a432373694fe08
David-ctrl-max <brightoil10@gmail.com>
2025-08-30 14:20:17 +0900

    Create importer.py

---
+++ services/importer.py
@@ -0,0 +1,8 @@
+# services/importer.py
+import logging
+
+def run_all():
+    logging.info("[run_all] 실제 SEO/임포트 작업 시작")
+    # TODO: 여기 실제 작업 넣기
+    # 예) fetch_products(); generate_meta(); submit_sitemap(); ...
+    logging.info("[run_all] 실제 SEO/임포트 작업 완료")
